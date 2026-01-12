0cbe3223c78949b0cab7a376456c635ecd688799
Nayara Milagres <nayaramilagri@gmail.com>
2026-01-12 15:41:25 -0300

    Delete LiterAlura directory

--- LiterAlura/src/ConsumoApi.java
+++
@@ -1,7 +0,0 @@
-@Service
-public class ConsumoApi {
-
-    public String obterDados(String endereco) {
-        return new RestTemplate().getForObject(endereco, String.class);
-    }
-}
--- LiterAlura/src/LivroRepository.java
+++
@@ -1,5 +0,0 @@
-import java.util.List;
-
-public interface LivroRepository extends JpaRepository<Livro, Long> {
-    List<Livro> findByIdiomaIgnoreCase(String idioma);
-}
